d4b980a843d61c086ec43fdcbb2bdd130f9dffdd
Sifa Salafiah <sifa.salafiah@uii.ac.id>
2021-12-25 13:15:54 +0700

    add component service provider

---
+++ src/Providers/ComponentServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Codakarta\Aru2ka\Providers\ComponentServiceProvider;
+
+use Illuminate\Support\ServiceProvider;
+
+class ComponentServiceProvider extends ServiceProvider {
+
+    public function boot()
+    {
+
+    }
+    public function register()
+    {
+        
+    }
+}
+?>
